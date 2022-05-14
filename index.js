129ec2ee22b2d9146625e2d8bb28d6131244d484
MK Work <mk.hcj2@gmail.com>
2022-05-14 12:48:42 +0900

    2nd

--- index.js
+++ index.js
@@ -1,4 +1,44 @@
 let numc = document.getElementById("numberC");
+let incrementNum = 0;
+document.getElementById("buttonA").onclick = function increaser() {
+    incrementNum = incrementNum + 1;
+    numc.innerHTML= incrementNum;
+}
+
+let titleM = document.getElementById("title");
+function submit() {
+    titleM = titleM.value;
+    let yourNAme = document.getElementById("yourName").value;
+    let ourAnswer = document.getElementById("answer");
+    ourAnswer.innerHTML = "hello! " + titleM +". " + yourNAme;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let numc = document.getElementById("numberC");
 
 let um = 0;
 document.getElementById("buttonA").onclick = function counten() {
@@ -6,7 +46,6 @@ document.getElementById("buttonA").onclick = function counten() {
     numc.innerHTML = um;
 }
 
-//name answer
 function submit() {
     let yourName = document.getElementById("yourName").value;
     let talk = document.getElementById("answer");
@@ -20,7 +59,7 @@ function submit() {
 
 
 
-/*document.getElementById("submit").onclick = function() {
+document.getElementById("submit").onclick = function() {
     let yourName = document.getElementById("yourName").value;
     let talk = document.getElementById("answer");
     talk.innerHTML= "Nice to meet you, " + yourName;
